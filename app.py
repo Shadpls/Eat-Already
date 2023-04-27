705d3ceca8e7d1b76dfe65cc9312f2e5218d5ed2
Shadpls <Shad.q.k@gmail.com>
2023-04-26 23:49:32 -0500

    old

--- app.py
+++ app.py
@@ -13,12 +13,11 @@ from flask_login import (
     logout_user,
     current_user,
 )
-from wtforms import StringField, PasswordField, SubmitField, IntegerField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import InputRequired, Length, ValidationError, NumberRange
 from wtforms.widgets import TextArea
 from flask_wtf import FlaskForm
 from flask_bcrypt import Bcrypt
-
 
 load_dotenv(find_dotenv())
 
@@ -90,21 +89,6 @@ class EatWhatForm(FlaskForm):
         validators=[InputRequired(), Length(min=2, max=30)],
         render_kw={"placeholder": "Enter your location (city, zip-code)"},
     )
-    category = SelectField(
-        validators=[InputRequired()],
-        choices=[
-            ("", "Type of food"),
-            ("italian", "Italian"),
-            ("chinese", "Chinese"),
-            ("mexican", "Mexican"),
-            ("american", "American"),
-            ("indian", "Indian"),
-            ("japanese", "Japanese"),
-            ("thai", "Thai"),
-            ("healthy", "Healthy"),
-        ],
-        render_kw={"placeholder": "Select a category"},
-    )
     # category = StringField(
     #     validators=[InputRequired(), Length(min=3, max=40)],
     #     render_kw={"placeholder": "Enter a category (e.g. Italian, Chinese, etc)"},
@@ -160,10 +144,17 @@ def search():
     form = EatWhatForm()
     if form.validate_on_submit():
         user_local = form.user_local.data
-        category = form.category.data
-        eat_this_dict = decide_4me(user_local, category)
+        # category = form.category.data
+
+        if not check_valid_location(user_local):
+            return redirect(url_for("search"))
+
+        eat_this_dict = decide_4me(user_local, "")
         session["name"] = eat_this_dict["Name"]
         session["addr"] = eat_this_dict["Address"]
+        session["image"] = eat_this_dict["Image"]
+        session["phone"] = eat_this_dict["Phone"]
+        session["url"] = eat_this_dict["URL"]
         return redirect(url_for("results"))
     return render_template("search.html", form=form)
 
@@ -173,34 +164,24 @@ def search():
 def results():
     name = session.get("name")
     addr = session.get("addr")
-    return render_template("results.html", name=name, addr=addr)
+    image = session.get("image")
+    phone = session.get("phone")
+    url = session.get("url")
+    return render_template(
+        "results.html", name=name, addr=addr, image=image, url=url, phone=phone
+    )
 
 
 def decide_4me(user_local, category):
     BASE_YELP_URL = "https://api.yelp.com/v3/businesses/search"
     HEADERS = {"Authorization": "Bearer %s" % getenv("FOOD_SELECTOR_API")}
     PARAMS = {
+        "term": "restaurant",
         "location": str(user_local),
         "term": category,
         "radius": 6500,  # need to convert miles to meters -> for submit field
-        "open_now": "",
         "limit": 50,  # depending on pure goal of app we want 1 or 3 #
     }
-
-    # yelp_response = requests.get(BASE_YELP_URL, params=PARAMS, headers=HEADERS)
-    # addr = yelp_response.json()["businesses"][0]["location"]["display_address"]
-    # img_url = yelp_response.json()["businesses"][0]["url"]
-    # phone = yelp_response.json()["businesses"][0]["display_phone"]
-    # rest_url = yelp_response.json()["businesses"][0]["url"]
-    # name = yelp_response, json()["businesses"][0]["name"]
-
-    # eat_this_dict = {
-    #     "Name": name,
-    #     "Address": addr,
-    #     "Image": img_url,
-    #     "Phone": phone,
-    #     "URL": rest_url,
-    # }
 
     yelp_response = requests.get(BASE_YELP_URL, params=PARAMS, headers=HEADERS)
 
@@ -208,12 +189,9 @@ def decide_4me(user_local, category):
 
     restaurant = random.choice(data["businesses"])
 
-    print(restaurant["name"])
-    print(restaurant["location"]["address1"])
-
     addr = restaurant["location"]["display_address"]
     name = restaurant["name"]
-    img_url = restaurant["url"]
+    img_url = restaurant["image_url"]
     phone = restaurant["display_phone"]
     rest_url = restaurant["url"]
 
@@ -225,6 +203,32 @@ def decide_4me(user_local, category):
         "URL": rest_url,
     }
     return eat_this_dict
+
+
+def check_valid_location(location):
+    BASE_YELP_URL = "https://api.yelp.com/v3/businesses/search"
+    HEADERS = {"Authorization": "Bearer %s" % getenv("FOOD_SELECTOR_API")}
+    PARAMS = {"location": location, "term": "food"}
+
+    response = requests.get(BASE_YELP_URL, params=PARAMS, headers=HEADERS)
+
+    if response.ok:
+        data = response.json()
+        if data.get("businesses"):
+            return True  # Location is a valid city name
+
+    BASE_ZIP_URL = "https://www.zipcodeapi.com/rest/%s/info.json/%s/degrees" % (
+        getenv("ZIPCODE_API_KEY"),
+        location,
+    )
+
+    response = requests.get(BASE_ZIP_URL)
+    if response.ok:
+        data = response.json()
+        if data.get("city"):
+            return True  # Location is a valid zip code
+
+    return False  # Location is not valid city or zip code
 
 
 # Uncomment to run locally comment back to deploy
